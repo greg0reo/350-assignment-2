06ab7d990a2e89b62ef170f53d382af2ad0825c7
Grigoriy Redozubov <gredozu1@binghamton.edu>
2019-02-14 15:46:15 -0500

    Wowee, I'll have some brie

--- assignment2/prog2tree.c
+++ assignment2/prog2tree.c
@@ -1,2 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+/* So the way this'll work:
+
+	when level = 1, either sleep or paush, depending on relevant tag
+	processes should be called in bredth first order
+
+ */
